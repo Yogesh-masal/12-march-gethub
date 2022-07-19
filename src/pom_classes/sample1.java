8150936159c63f45807ce68247ced827b7570fbc
Admin123 <Admin123@DESKTOP-K5U87LI>
2022-07-19 20:02:17 +0530

    [yogesh]:added sample1

---
+++ src/pom_classes/sample1.java
@@ -0,0 +1,20 @@
+package pom_classes;
+
+public class sample1
+{
+public static void main(String[] args)
+{
+
+	System.out.println("hello");
+	
+	System.out.println("i am yogesh");
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
